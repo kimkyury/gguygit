3bbdd24e43e54a0dcd0c77ed4bf07f9218faa0ad
KGYURY <71677810+kimkyury@users.noreply.github.com>
2024-06-25 17:51:36 +0900

    chore: Allow all access

--- Backend/Chat/src/main/java/org/bnksys/chat/configs/WebSocketConfig.java
+++ Backend/Chat/src/main/java/org/bnksys/chat/configs/WebSocketConfig.java
@@ -12,12 +12,12 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOrigins("http://localhost:3000").withSockJS();
+        registry.addEndpoint("/ws").setAllowedOrigins("*").withSockJS();
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic");
         config.setApplicationDestinationPrefixes("/app");
+        config.enableSimpleBroker("/topic");
     }
 }
