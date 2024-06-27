cfa1a9e52f8db83e61bed8d3e907a10e433c3c83
김규리 <mandarining0918@gmail.com>
2024-06-27 21:17:31 +0900

    Feat: Create Entities

--- Backend/Chat2/src/main/java/org/bnksys/chat/models/ChatMessage.java
+++ Backend/Chat2/src/main/java/org/bnksys/chat/models/ChatMessage.java
@@ -1,5 +1,17 @@
 package org.bnksys.chat.models;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ChatMessage {
 
+    private String type;
+    private String content;
+    private String sender;
+    private Long chatRoomId;
 }
