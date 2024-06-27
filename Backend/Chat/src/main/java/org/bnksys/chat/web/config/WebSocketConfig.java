9d2cfb216ffd497beee788626f6a9a8468023ade
KGYURY <71677810+kimkyury@users.noreply.github.com>
2024-06-27 14:10:06 +0900

    Feat: Setting CORS

--- Backend/Chat/src/main/java/org/bnksys/chat/web/config/WebSocketConfig.java
+++ Backend/Chat/src/main/java/org/bnksys/chat/web/config/WebSocketConfig.java
@@ -1,4 +1,4 @@
-package org.bnksys.chat.configs;
+package org.bnksys.chat.web.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
@@ -12,12 +12,14 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOrigins("http://localhost:3000").withSockJS();
+        registry.addEndpoint("/ws")
+                .setAllowedOriginPatterns("*")
+                .withSockJS();
     }
 
     @Override
-    public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.setApplicationDestinationPrefixes("/app");
-        config.enableSimpleBroker("/topic");
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/pub");
+        registry.enableSimpleBroker("/sub");
     }
 }
