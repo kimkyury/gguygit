48956e24ed1a00c1362259c690cd99130709c655
KGYURY <mandarining0918@gmail.com>
2024-12-12 15:31:54 +0900

    Feat: Config CORS

--- Go-Back/network/network.go
+++ Go-Back/network/network.go
@@ -3,6 +3,7 @@ package network
 import (
 	"log"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -13,6 +14,17 @@ type Network struct {
 // Network가 사용할 수 있는 메소드다
 func NewServer() *Network {
 	n := Network{engin: gin.New()}
+
+	n.engin.Use(gin.Logger())   // 로그를 찍어라
+	n.engin.Use(gin.Recovery()) // 서버 장애 시 자동으로 failover?
+	n.engin.Use(cors.New(cors.Config{
+		AllowWebSockets:  true,
+		AllowOrigins:     []string("*"),
+		AllowMethod:      []string("GET", "POST", "PUT"),
+		AllowHeaders:     []string("*"),
+		AllowCredentials: true,
+	}))
+
 	return &n
 }
 
