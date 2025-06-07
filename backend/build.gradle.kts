6aff48de7c81fe702951730e199670016458206e
김규리 <mandarining0918@gmail.com>
2025-06-07 22:39:23 +0900

    fix: downgrade java24 -> java21

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -19,8 +19,14 @@ repositories {
 }
 
 dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-web")
+
 	implementation("org.springframework.boot:spring-boot-starter")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+//	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
+
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
