48f559221362ab7f6c4bd35e612caddaaba4a1a8
김규리 <mandarining0918@gmail.com>
2025-06-07 21:51:19 +0900

    backend-projcet init

--- Before/Backend/Chat2/src/main/java/org/bnksys/chat/repositories/UserChatRoomEventRepository.java
+++
@@ -1,7 +0,0 @@
-package org.bnksys.chat.repositories;
-
-import org.bnksys.chat.entities.UserChatRoomEvent;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserChatRoomEventRepository extends JpaRepository<UserChatRoomEvent, Long> {
-}
