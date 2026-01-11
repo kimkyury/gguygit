645ffa63322044b2a7a6d0b280e52c4cedf41b63
김규리 <mandarining0918@gmail.com>
2026-01-11 23:11:32 +0900

    initialize

--- backend/src/main/kotlin/com/kgyury/gguigit/dto/MusicPostCreateReq.kt
+++
@@ -1,9 +0,0 @@
-package com.kgyury.gguigit.dto
-
-data class MusicPostCreateReq(
-    val text: String,
-    val videoUrl: String,
-    val imageUrl: String,
-    val author: String,
-    val rating: Int
-)
--- backend/src/main/kotlin/com/kgyury/gguigit/repository/MusicPostRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.kgyury.gguigit.repository
-
-import com.kgyury.gguigit.domain.MusicPost
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface MusicPostRepository : JpaRepository<MusicPost, Long>{
-
-}
